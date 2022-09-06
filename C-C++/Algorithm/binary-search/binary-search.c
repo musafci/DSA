850e20a8598b03975a99218e597f56822a76153e
Md. Abu Musa <musa.fci@gmail.com>
2022-09-06 20:22:50 +0600

    Commit#2 Binary Search Algorithm

--- C-C++/Algorithm/binary-search/binary-search.c
+++ C-C++/Algorithm/binary-search/binary-search.c
@@ -1,3 +1,14 @@
+// Step-by-step Binary Search Algorithm: We basically ignore half of the elements just after one comparison.
+
+// Compare x with the middle element.
+// If x matches with the middle element, we return the mid index.
+// Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. 
+// So we recur for the right half.
+// Else (x is smaller) recur for the left half.
+
+
+// Iteration Method
+
 #include <stdio.h>
 
 int binary_search( int arr[], int n, int x ) {
@@ -38,3 +49,6 @@ int main() {
     
     return 0;
 }
+
+// Time Complexity: O(log n)
+// Space Complexity: O(log n)
